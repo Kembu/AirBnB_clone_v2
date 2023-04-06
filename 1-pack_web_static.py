a39168696625ef0c4c14491757a19bf139f5f8bd
Kembu <allantoo05@gmail.com>
2023-04-06 12:23:31 -0700

    pycodestyle_fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,20 +3,21 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """ a method to compress a file and return it's path """
-    
+
     """saving the current timestamp and creatinf filename"""
     time_now = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(time_now)
-   
+
     try:
         """create directory versions"""
         local("mkdir -p versions")
-         
+
         """create archive file"""
         local("tar -cvzf {} web_static/".format(file_path))
-        
+
         """return the path to the archive file created"""
         return "{}".format(file_path)
 
