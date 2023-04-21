297760b7f184717007b759ee10573a7eea240292
Kembu <allantoo05@gmail.com>
2023-04-21 00:39:17 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,20 +3,25 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """Function to define index"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """Funtion to define index/hbnb"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """Function to define /C/txt"""
     return f'C {text.replace("_", " ")}'
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000)
