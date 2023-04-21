7cf88e9f79acbf30c5e338f1ee2ab9921cddb064
Kembu <allantoo05@gmail.com>
2023-04-21 01:30:41 -0700

    pycodestyle_fstring

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,20 +3,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """Function to define index"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """Funtion to define index/hbnb"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """Function to define /C/txt"""
-    return f'C {text.replace("_", " ")}'
+    return 'C ' + text.replace("_", " ")
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -24,15 +29,18 @@ def python_is_cool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
     """display only if n is number"""
-    return f'{n} is a number'
+    return '{n} is a number'.format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def title_number(n):
     """display HTML page if n is a number"""
     return render_template('5-number.html', number=n)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000)
