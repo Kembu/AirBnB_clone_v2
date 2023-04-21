14505cbb26dcd25a84abed8ba19bfdb0f85ceb4e
Kembu <allantoo05@gmail.com>
2023-04-21 01:08:19 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def index_hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """Function to define /C/txt"""
-    return 'C' + {text.replace("_", " ")}
+    return 'C '+ {text.replace("_", " ")}
 
 
 if __name__ == "__main__":
